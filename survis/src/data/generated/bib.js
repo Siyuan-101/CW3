4cfb4f06305bc46dda0723ecb9c58f26c5c3520c
Siyuan-101 <psxsl28@nottingham.ac.uk>
2024-05-17 15:53:23 +0100

    Add files via upload

---
+++ survis/src/data/generated/bib.js
@@ -0,0 +1,111 @@
+﻿const generatedBibEntries = {
+    "bentley2005two": {
+        "author": "Bentley, Peter J and Greensmith, Julie and Ujjin, Supiya",
+        "booktitle": "Lecture Notes in Computer Science (including subseries Lecture Notes in Artificial Intelligence and Lecture Notes in Bioinformatics)",
+        "doi": "10.1007/11536444_11",
+        "pages": "139--152",
+        "publisher": "Springer",
+        "title": "Two ways to grow tissue for artificial immune systems",
+        "type": "incollection",
+        "volume": "3627",
+        "year": "2005"
+    },
+    "castro2002artificial": {
+        "author": "Castro, Leandro Nunes De and Timmis, Jon",
+        "doi": "10.1109/CEC.2002.1007011",
+        "journal": "Proceedings of the IEEE Congress on Evolutionary Computation",
+        "pages": "699--704",
+        "publisher": "IEEE",
+        "title": "An artificial immune network for multimodal function optimization",
+        "type": "article",
+        "year": "2002"
+    },
+    "gorris2018eight": {
+        "author": "Gorris, Mark AJ and Halilovic, Altuna and Rabold, Katrin and van Duffelen, Anne and Wickramasinghe, Iresha N and Verweij, Dagmar and Wortel, Inge and Textor, Johannes C and de Vries, I Jolanda M and Figdor, Carl G",
+        "doi": "10.4049/jimmunol.1701262",
+        "journal": "The Journal of Immunology",
+        "number": "1",
+        "pages": "347--354",
+        "title": "Eight-color multiplex immunohistochemistry for simultaneous detection of multiple immune checkpoint molecules within the tumor microenvironment",
+        "type": "article",
+        "volume": "200",
+        "year": "2018"
+    },
+    "greensmith2009artificial": {
+        "author": "Greensmith, Julie and Aickelin, Uwe",
+        "booktitle": "Lecture Notes in Computer Science (including subseries Lecture Notes in Artificial Intelligence and Lecture Notes in Bioinformatics)",
+        "doi": "10.1007/978-3-540-92916-1_16",
+        "pages": "89--103",
+        "publisher": "Springer",
+        "title": "Artificial dendritic cells: multi-faceted perspectives",
+        "type": "incollection",
+        "volume": "5666",
+        "year": "2009"
+    },
+    "greensmith2010artificial": {
+        "author": "Greensmith, Julie and Whitbrook, Amanda and Aickelin, Uwe",
+        "booktitle": "Artificial immune systems: Advances in research and applications",
+        "doi": "10.1007/978-3-642-33757-4",
+        "pages": "1--20",
+        "publisher": "Springer",
+        "title": "An artificial immune network for multimodal function optimization",
+        "type": "incollection",
+        "year": "2010"
+    },
+    "greensmith2016exploiting": {
+        "author": "Greensmith, Julie and Jackson, Andrew M and Spendlove, Ian",
+        "booktitle": "ACM Transactions on Autonomous and Adaptive Systems",
+        "doi": "10.1145/2908961.2908962",
+        "number": "2",
+        "pages": "11",
+        "publisher": "ACM",
+        "title": "Exploiting the plasticity of primary and secondary response mechanisms in artificial immune systems",
+        "type": "incollection",
+        "volume": "11",
+        "year": "2016"
+    },
+    "gu2011quiet": {
+        "author": "Gu, Feng and Feyereisl, Jan and Oates, Robert and Reps, Jenna and Greensmith, Julie",
+        "booktitle": "Lecture Notes in Computer Science (including subseries Lecture Notes in Artificial Intelligence and Lecture Notes in Bioinformatics)",
+        "doi": "10.1007/978-3-642-22371-6_17",
+        "pages": "173--187",
+        "publisher": "Springer",
+        "title": "Quiet in class: classification, noise and the dendritic cell algorithm",
+        "type": "incollection",
+        "volume": "6825",
+        "year": "2011"
+    },
+    "gu2012dendritic": {
+        "author": "Gu, Feng and Greensmith, Julie and Aickelin, Uwe",
+        "booktitle": "Information Security Technical Report",
+        "doi": "10.4018/978-1-61350-092-7.ch005",
+        "pages": "58--64",
+        "publisher": "Elsevier",
+        "title": "The dendritic cell algorithm for intrusion detection",
+        "type": "incollection",
+        "volume": "17",
+        "year": "2012"
+    },
+    "watkins2004artificial": {
+        "author": "Watkins, Andrew and Timmis, Jon and Boggess, Lois",
+        "booktitle": "Genetic Programming and Evolvable Machines",
+        "doi": "10.1023/B:GENP.0000030197.83685.94",
+        "number": "3",
+        "pages": "291--317",
+        "title": "Artificial immune recognition system (AIRS): An immune-inspired supervised learning algorithm",
+        "type": "inproceedings",
+        "volume": "5",
+        "year": "2004"
+    },
+    "whitbrook2005artificial": {
+        "author": "Whitbrook, Amanda and Greensmith, Julie and Aickelin, Uwe",
+        "booktitle": "IET Conference Publications",
+        "doi": "10.1049/cp:20050376",
+        "number": "5",
+        "pages": "295--300",
+        "publisher": "IET",
+        "title": "Artificial Immune Systems",
+        "type": "incollection",
+        "year": "2005"
+    }
+};
